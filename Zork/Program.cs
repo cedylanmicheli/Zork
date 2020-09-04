2c88ce78b346444f026bb0a5d87713410ffa3758
cedylanmicheli <cedylanmicheli@gmail.com>
2020-09-03 21:17:18 -0400

    Zork 1.3 - The Game Loop

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,22 +8,33 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
+            while(command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
 
-            //if (inputString == "QUIT") 
-            //{
-            //    Console.WriteLine("Thank you for playing.");
-            //}
-            //else if (inputString == "LOOK") 
-            //{
-            //    Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork' lies by the door.");
-            //}
-            //else 
-            //{
-            //    Console.Write("Unrecognized command.");
-            //}
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing.";
+                        break;
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork' lies by the door.";
+                        break;
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        outputString = $"You moved {command}.";
+                        break;
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
+                Console.WriteLine(outputString);
+            }       
         }
 
         private static Commands ToCommand(string commandString)
